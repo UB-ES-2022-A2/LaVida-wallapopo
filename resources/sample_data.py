8067394ec0082040dcec81df7dcf245de9864b4c
jorgec4444 <vinagre444@gmail.com>
2022-10-10 20:42:39 +0200

    First look: Hardcoded data for the products. Still have to discuss which elements we should add or remove

---
+++ resources/sample_data.py
@@ -0,0 +1,42 @@
+products = [
+    {'id': 0,
+     'name': "Oso de peluche",
+     'price': 4,
+     'category': "juguetes",  # Future impl: make a list of different categories for a single product cat:[]
+     'description': "oso de peluche viejo pero en buen estado",
+     'date': "04-oct-2022",  # or in number format
+     'status': "new",
+     'image': "https://media.istockphoto.com/photos/toy-teddy-bear-isolated-on-white-picture-id525900055?k=20&m=525900055&s=612x612&w=0&h=y_s6ycltdbuKM_yCggydHtpvw_xjWBe8B2FJ_HGPAGk=",  # same as categories imgs:[]
+     # location:¿?
+     },
+    {'id': 1,
+     'name': "parchis",
+     'price': 20,
+     'category': "juegos de mesa",  # Future impl: make a list of different categories for a single product cat:[]
+     'description': "juego de parchis edición retro",
+     'date': "24-apr-2021",  # or in number format
+     'status': "almost new",
+     'image': "https://cloud10.todocoleccion.online/juegos-mesa/tc/2016/03/15/20/55059706.jpg",  # same as categories
+     # location:¿?
+     },
+    {'id': 2,
+     'name': "seat ibiza 2006",
+     'price': 5000,
+     'category': "coches",  # Future impl: make a list of different categories for a single product cat:[]
+     'description': "seat ibiza del año 2006, tira como el primer día, asientos de atrás quitados y el aire acondicionado no funciona",
+     'date': "09-dic-2020",  # or in number format
+     'status': "used",
+     'image': "https://img.remediosdigitales.com/ebec4d/seat_ibiza_2006-1/450_1000.jpg",
+     # location:¿?
+     },
+    {'id': 3,
+     'name': "vajilla",
+     'price': 20,
+     'category': "home",  # Future impl: make a list of different categories for a single product cat:[]
+     'description': "vajilla de platos de mi abuela, se han utilizado un par de veces",
+     'date': "10-oct-2022",  # or in number format
+     'status': "almost new",
+     'image': "http://m.anticuarium.es/image/productos/46325A5C-3AA3-F04B-CC11-8E5097F786DB.jpg",  # same as categories imgs:[]
+     # location:¿?
+     }
+]
