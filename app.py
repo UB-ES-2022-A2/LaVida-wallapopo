52f998f96cc5802eebe7cdc0f6a87850eb7db60c
Chang Ye <1020994740a@gmail.com>
2022-10-11 00:32:41 +0200

    Account skeleton done.
    For now, each account must have:
    - Email (primary key for now, might change it later)
    - Password
    - Role (regular user and admin)
    
    Optional account info:
    - name
    - surname
    - birthday
    - profile picture url

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from flask_migrate import Migrate
+from db import db
+from models.accounts import AccountsModel
 
 app = Flask(__name__)
+
+# config used for now, will be changed later on
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello Worldeeeeee!'
+    return 'Hello World!'
 
 
 if __name__ == '__main__':
---
+++ db.py
@@ -0,0 +1,7 @@
+from flask_sqlalchemy import SQLAlchemy
+from geoalchemy2 import Geometry
+# flask db init
+# flask db migrate -m "Initial migration".
+# flask db upgrade
+secret_key = "kdsfklsmfakfmafmadslvsdfasdf"
+db = SQLAlchemy()
