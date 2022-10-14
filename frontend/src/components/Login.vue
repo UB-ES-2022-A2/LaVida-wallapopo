4e5d4d65ff7c9cee75cbbbbe4ee90c39f80aff7c
Dani <61388065+computingcreeper@users.noreply.github.com>
2022-10-14 16:02:20 +0200

    US3.1
    Afegeixo la part visual de la pagina de registre corresponent a la US3.1

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,25 +1,43 @@
 <template>
   <div id="login">
     <div v-if="creatingAccount" class="col d-flex justify-content-center">
-      <div class="card" style="width: 30%;">
+      <!-- Card registro -->
+      <div class="card bg-light" style="width: 30%;">
         <div class="card-body">
-          <h5 class="card-title" align="center">Create Account</h5>
+          <h5 class="card-title" align="center">Registrate en Wallapopo</h5>
           <div class="container">
             <b-card-text>
               <div class="form-label-group">
-                <label for="inputEmail">Email</label>
-                <input type="email" id="inputEmail" class="form-control"
-                placeholder="Email" required autofocus v-model="addUserForm.email">
+                <br>
+                <input type="text" id="inputName" class="form-control" placeholder="Nombre y apellidos" required
+                  v-model="addUserForm.username">
               </div>
               <div class="form-label-group">
                 <br>
-                <label for="inputPassword">Password</label>
-                <input type="password" id="inputPassword" class="form-control"
-                placeholder="Password" required v-model="addUserForm.password">
+                <input type="email" id="inputEmail" class="form-control" placeholder="Dirección de email" required
+                  autofocus v-model="addUserForm.email">
+              </div>
+              <div class="form-label-group">
+                <br>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required
+                  minlength="8" maxlength="20" v-model="addUserForm.password" @change="checkPassword(this)">
+              </div>
+              <div class="form-label-group">
+                <br>
+                <input type="password" id="inputPassword2" class="form-control" placeholder="Verificar contraseña"
+                  required minlength="8" maxlength="20" v-model="addUserForm.password2">
+              </div>
+              <div>
+                <br>
+                <b-form-checkbox id="checkbox-1" v-model="status_policy" name="checkbox-1" value="1"
+                  unchecked-value="0">
+                  He leído y acepto las Condiciones de uso y la Política de privacidad de Wallapopo.
+                </b-form-checkbox>
               </div>
             </b-card-text>
-            <button class="btn btn-primary btn-lg my-2" @click="createAccount()" style="width: 100%;">Submit</button>
-            <button class="btn btn-secondary btn-lg my-2" @click="creatingAccount= false" style="width: 100%;">Back To Log In</button>
+            <button class="btn btn-primary btn-lg my-2" style="width: 100%;">Crear una
+              cuenta</button>
+            <button class="btn btn-secondary btn-lg my-2" @click="creatingAccount= false" style="width: 100%;">Cancelar</button>
           </div>
         </div>
       </div>
@@ -33,19 +51,21 @@
               <b-card-text>
                 <div class="form-label-group">
                   <label for="inputEmail2">Email</label>
-                  <input type="email" id="inputEmail2" class="form-control"
-                  placeholder="Email" required autofocus v-model="email">
+                  <input type="email" id="inputEmail2" class="form-control" placeholder="Email" required autofocus
+                    v-model="email">
                 </div>
                 <div class="form-label-group">
                   <br>
                   <label for="inputPassword2">Password</label>
-                  <input type="password" id="inputPassword2" class="form-control"
-                  placeholder="Password" required v-model="password">
+                  <input type="password" id="inputPassword2" class="form-control" placeholder="Password" required
+                    v-model="password">
                 </div>
               </b-card-text>
               <button class="btn btn-primary btn-lg my-2" @click="checkLogin()" style="width: 100%;">Sign In</button>
-              <button class="btn btn-success btn-lg my-2" @click="creatingAccount= true" style="width: 100%;">Create Account</button>
-              <button class="btn btn-secondary btn-lg my-2" @click="redirectToHome()" style="width: 100%;">Back To Main</button>
+              <button class="btn btn-success btn-lg my-2" @click="creatingAccount= true" style="width: 100%;">Create
+                Account</button>
+              <button class="btn btn-secondary btn-lg my-2" @click="redirectToHome()" style="width: 100%;">Back To
+                Main</button>
             </div>
           </div>
         </div>
@@ -61,10 +81,13 @@ export default {
       username: null,
       password: null,
       creatingAccount: false,
+      status_policy: '0',
       actualPath: 'http://localhost:5000/',
       addUserForm: {
         username: null,
-        password: null
+        email: null,
+        password: null,
+        password2: null
       }
     }
   },
