d9d12894f527bdc47cf3733f7ce663a062a6af89
SirKaza <mcasanto14@alumnes.ub.edu>
2022-10-17 21:11:45 +0200

    login with forgot password

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -3,23 +3,43 @@
     <div v-if="creatingAccount" class="col d-flex justify-content-center">
       <div class="card" style="width: 30%;">
         <div class="card-body">
-          <h5 class="card-title" align="center">Create Account</h5>
+          <h5 class="card-title" align="center">Crear Cuenta</h5>
           <div class="container">
             <b-card-text>
               <div class="form-label-group">
-                <label for="inputEmail">Email</label>
+                <label for="inputEmail">Correo</label>
                 <input type="email" id="inputEmail" class="form-control"
-                placeholder="Email" required autofocus v-model="addUserForm.email">
+                placeholder="Introducir correo" required autofocus v-model="addUserForm.email">
               </div>
               <div class="form-label-group">
                 <br>
-                <label for="inputPassword">Password</label>
+                <label for="inputPassword">Contraseña</label>
                 <input type="password" id="inputPassword" class="form-control"
-                placeholder="Password" required v-model="addUserForm.password">
+                placeholder="Introducir contraseña" required v-model="addUserForm.password">
               </div>
             </b-card-text>
-            <button class="btn btn-primary btn-lg my-2" @click="createAccount()" style="width: 100%;">Submit</button>
-            <button class="btn btn-secondary btn-lg my-2" @click="creatingAccount= false" style="width: 100%;">Back To Log In</button>
+            <button class="btn btn-primary btn-lg my-2" @click="createAccount()" style="width: 100%;">Enviar</button>
+            <button class="btn btn-secondary btn-lg my-2" @click="creatingAccount= false" style="width: 100%;">Volver</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else-if="forgotPassword" class="col d-flex justify-content-center">
+      <div class="card" style="width: 30%;">
+        <div class="card-body">
+          <h5 class="card-title" align="center">Cambiar Contraseña</h5>
+          <div class="container">
+            <div>
+              <b-card-text>
+                <div class="form-label-group">
+                  <label for="inputEmail3">Correo</label>
+                  <input type="email" id="inputEmail3" class="form-control"
+                  placeholder="Introducir correo" required autofocus v-model="email">
+                </div>
+              </b-card-text>
+              <button class="btn btn-primary btn-lg my-2" @click="forgotPassword()" style="width: 100%;">Enviar</button>
+              <button class="btn btn-secondary btn-lg my-2" @click="forgotPassword= false" style="width: 100%;">Volver</button>
+            </div>
           </div>
         </div>
       </div>
@@ -27,25 +47,26 @@
     <div v-else class="col d-flex justify-content-center">
       <div class="card" style="width: 30%;">
         <div class="card-body">
-          <h5 class="card-title" align="center">Sign In</h5>
+          <h5 class="card-title" align="center">Iniciar Sesión</h5>
           <div class="container">
             <div>
               <b-card-text>
                 <div class="form-label-group">
-                  <label for="inputEmail2">Email</label>
+                  <label for="inputEmail2">Correo</label>
                   <input type="email" id="inputEmail2" class="form-control"
-                  placeholder="Email" required autofocus v-model="email">
+                  placeholder="Introducir correo" required autofocus v-model="email">
                 </div>
                 <div class="form-label-group">
                   <br>
-                  <label for="inputPassword2">Password</label>
+                  <label for="inputPassword2">Contraseña</label>
                   <input type="password" id="inputPassword2" class="form-control"
-                  placeholder="Password" required v-model="password">
+                  placeholder="Introducir contraseña" required v-model="password">
                 </div>
               </b-card-text>
-              <button class="btn btn-primary btn-lg my-2" @click="checkLogin()" style="width: 100%;">Sign In</button>
-              <button class="btn btn-success btn-lg my-2" @click="creatingAccount= true" style="width: 100%;">Create Account</button>
-              <button class="btn btn-secondary btn-lg my-2" @click="redirectToHome()" style="width: 100%;">Back To Main</button>
+              <p class="text-center" @click="forgotPassword= true">¿Has olvidado la contraseña?</p>
+              <button class="btn btn-primary btn-lg my-2" @click="checkLogin()" style="width: 100%;">Entrar</button>
+              <button class="btn btn-success btn-lg my-2" @click="creatingAccount= true" style="width: 100%;">Registrarme</button>
+              <button class="btn btn-secondary btn-lg my-2" @click="redirectToHome()" style="width: 100%;">Volver</button>
             </div>
           </div>
         </div>
@@ -58,12 +79,13 @@
 export default {
   data () {
     return {
-      username: null,
+      email: null,
       password: null,
       creatingAccount: false,
+      forgotPassword: false,
       actualPath: 'http://localhost:5000/',
       addUserForm: {
-        username: null,
+        email: null,
         password: null
       }
     }
