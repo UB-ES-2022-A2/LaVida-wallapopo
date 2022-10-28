1a1f00c15df2327c77fdf13700ff7abd7cdab0b5
SirKaza <mcasanto14@alumnes.ub.edu>
2022-10-28 23:47:14 +0200

    footer acabado

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,111 +1,67 @@
 <template>
   <div id="footer">
     <!-- Footer -->
-    <footer class="text-center text-lg-start bg-white text-muted">
-      <!-- Section: Social media -->
-      <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-        <!-- Left -->
-        <div class="me-5 d-none d-lg-block">
-          <span>Get connected with us on social networks:</span>
-        </div>
-        <!-- Left -->
-
-        <!-- Right -->
-        <div>
-          <a href="" class="me-4 link-secondary">
-            <i class="fab fa-facebook-f"></i>
-          </a>
-          <a href="" class="me-4 link-secondary">
-            <i class="fab fa-twitter"></i>
-          </a>
-          <a href="" class="me-4 link-secondary">
-            <i class="fab fa-google"></i>
-          </a>
-          <a href="" class="me-4 link-secondary">
-            <i class="fab fa-instagram"></i>
-          </a>
-          <a href="" class="me-4 link-secondary">
-            <i class="fab fa-linkedin"></i>
-          </a>
-          <a href="" class="me-4 link-secondary">
-            <i class="fab fa-github"></i>
-          </a>
-        </div>
-        <!-- Right -->
-      </section>
-      <!-- Section: Social media -->
-
+    <footer class="text-center text-lg-start bg-light text-muted">
       <!-- Section: Links  -->
       <section class="">
-        <div class="container text-center text-md-start mt-5">
+        <div class="d-flex justify-content-center justify-content-lg-between container text-center text-md-start mt-5 p-4 border-bottom">
           <!-- Grid row -->
-          <div class="row mt-3">
+          <div class="col d-flex justify-content-center">
+
             <!-- Grid column -->
-            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-              <!-- Content -->
+            <div class="mx-auto mb-4">
+              <!-- Links -->
               <h6 class="text-uppercase fw-bold mb-4">
-                <i class="fas fa-gem me-3 text-secondary"></i>Company name
+                Wallapopo
               </h6>
               <p>
-                Here you can use rows and columns to organize your footer content. Lorem ipsum
-                dolor sit amet, consectetur adipisicing elit.
+                <a href="#!" class="text-reset">Quiénes somos</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Como funciona</a>
               </p>
             </div>
             <!-- Grid column -->
 
             <!-- Grid column -->
-            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <div class="mx-auto mb-4">
               <!-- Links -->
               <h6 class="text-uppercase fw-bold mb-4">
-                Products
+                ¿Necesitas ayuda?
               </h6>
               <p>
-                <a href="#!" class="text-reset">Angular</a>
+                <a href="#!" class="text-reset">Preguntas frecuentes</a>
               </p>
               <p>
-                <a href="#!" class="text-reset">React</a>
-              </p>
-              <p>
-                <a href="#!" class="text-reset">Vue</a>
-              </p>
-              <p>
-                <a href="#!" class="text-reset">Laravel</a>
+                <a href="#!" class="text-reset">Contactar</a>
               </p>
             </div>
             <!-- Grid column -->
 
             <!-- Grid column -->
-            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <div class="mx-auto mb-4">
               <!-- Links -->
               <h6 class="text-uppercase fw-bold mb-4">
-                Useful links
+                Legal
               </h6>
               <p>
-                <a href="#!" class="text-reset">Pricing</a>
+                <a href="#!" class="text-reset">Condiciones de uso</a>
               </p>
               <p>
-                <a href="#!" class="text-reset">Settings</a>
-              </p>
-              <p>
-                <a href="#!" class="text-reset">Orders</a>
-              </p>
-              <p>
-                <a href="#!" class="text-reset">Help</a>
+                <a href="#!" class="text-reset">Política de privacidad</a>
               </p>
             </div>
             <!-- Grid column -->
 
             <!-- Grid column -->
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <div class="mx-auto mb-4">
               <!-- Links -->
-              <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-              <p><i class="fas fa-home me-3 text-secondary"></i> New York, NY 10012, US</p>
+              <h6 class="text-uppercase fw-bold mb-4">
+                Para profesionales
+              </h6>
               <p>
-                <i class="fas fa-envelope me-3 text-secondary"></i>
-                info@example.com
+                <a href="#!" class="text-reset">Wallapopo PRO</a>
               </p>
-              <p><i class="fas fa-phone me-3 text-secondary"></i> + 01 234 567 88</p>
-              <p><i class="fas fa-print me-3 text-secondary"></i> + 01 234 567 89</p>
             </div>
             <!-- Grid column -->
           </div>
@@ -115,9 +71,9 @@
       <!-- Section: Links  -->
 
       <!-- Copyright -->
-      <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025);">
-        © 2021 Copyright:
-        <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+      <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+        © 2022 Copyright:
+        <a class="text-reset fw-bold" href="">Wallapopo</a>
       </div>
       <!-- Copyright -->
     </footer>
