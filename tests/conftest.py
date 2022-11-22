425ee4e3715c6cddb78900801c067eaf4822d310
Pol Costa <polct1999@gmail.com>
2022-11-22 19:32:15 +0100

    Refactor setup de los tests de back para que no afecten la DB de development + eliminada la dependencia de algunos tests que necesitaban ejecutarse seguidos

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,68 @@
 import pytest
 
 import requests
+from app import app
+from db import db
+import random as rand
+from sqlalchemy import exc
+import resources.sample_data as data
+
 from requests.auth import HTTPBasicAuth
 from models.accounts import AccountsModel
 from models.products import ProductsModel
 
 
+def populate_db():
+    products = []
+    accounts = []
+
+    for product in data.products:
+        new_product = ProductsModel(name=product['name'], category=product['category'],
+                                    description=product['description'], price=product['price'],
+                                    condition=product['condition'])
+
+        new_product.image = product['image']
+        products.append(new_product)
+
+    for account in data.accounts:
+        new_account = AccountsModel(email=account['email'], username=account['username'],
+                                    confirmed=account['confirmed'])
+
+        new_account.hash_password(account['password'])
+        accounts.append(new_account)
+
+    # Relationship between products and user
+    for product in products:
+        user = rand.choice(accounts)
+        i = accounts.index(user)
+        product.user_id = accounts[i].email
+
+    try:
+        db.session.add_all(products)
+        db.session.add_all(accounts)
+        db.session.commit()
+    except exc.SQLAlchemyError:
+        db.session.rollback()
+
+
 # --------
 # Fixtures
 # --------
+@pytest.fixture()
+def _app():
+    with app.app_context():
+        db.create_all()
+        populate_db()
+        yield app
+        db.drop_all()
+
+
+@pytest.fixture()
+def client(_app):
+    from app import app
+    return app.test_client()
+
+
 @pytest.fixture(scope='function')
 def first_product():
     product = requests.get("http://localhost:5000/API/product/1")
@@ -37,18 +91,20 @@ def switch_product():
 
 
 @pytest.fixture(scope='function')
-def pepe_products():
-    p = ProductsModel.get_all_by_user("pepe432@gmail.com")
-    return p
+def pepe_products(_app):
+    with _app.app_context():
+        p = ProductsModel.get_all_by_user("pepe432@gmail.com")
+        return p
 
 
 @pytest.fixture(scope='function')
-def user_auth():
+def user_auth(_app, client):
     json = {'email': 'pepe432@gmail.com', 'password': 'pepe123,.'}
-    login = requests.post("http://localhost:5000/API/login", json=json)
-    token = login.json()
-    auth = HTTPBasicAuth(token['token'], '')
-    return auth
+    with _app.test_request_context():
+        login = client.post("API/login", data=json)
+        token = login.json
+        auth = HTTPBasicAuth(token['token'], '')
+        return auth
 
 
 @pytest.fixture(scope='function')
