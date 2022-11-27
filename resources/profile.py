3afccad9231e10f7d5a44be170e2b7f0d4fdb0d7
Pol Costa <polct1999@gmail.com>
2022-11-27 22:26:04 +0100

    APIs ver y editar perfil

--- resources/profile.py
+++ resources/profile.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+
+from db import db
 from models.accounts import AccountsModel, auth, g
 
 
@@ -14,6 +16,42 @@ class Profile(Resource):
 
         # return error if the account email doesn't match
         if account.username != g.user.username:
-            return {'message': "Bad authorization user"}, HTTPStatus.BAD_REQUEST
+            return {'message': "Bad authorization user"}, HTTPStatus.UNAUTHORIZED
 
         return {'account': account.json()}, HTTPStatus.OK
+
+    def post(self):
+        data = self.get_data()
+        email = data['email']
+        user = AccountsModel.get_by_email(email)
+        # return error message if account doesn't exist
+        if user is None:
+            return {'message': 'This email [{}] does not exist'.format(email)}, HTTPStatus.NOT_FOUND
+
+        try:
+            user.profile_picture = data['image']
+            user.name = data['name']
+            user.surname = data['surname']
+            # user.location = data['location']
+
+            db.session.add(user)
+            db.session.commit()
+
+        except Exception as e:
+            print(e)
+            return {'message': "Error updating the profile"}, HTTPStatus.INTERNAL_SERVER_ERROR
+
+        return {'message': "Profile updated successfully"}, HTTPStatus.OK
+
+    def get_data(self):
+        parser = reqparse.RequestParser()  # create parameters parser from request
+
+        # define all input parameters need and its type
+        parser.add_argument('email', type=str, required=True, help="This field cannot be left blank")
+        parser.add_argument('image', type=str, required=True, help="This field cannot be left blank")
+        parser.add_argument("name", type=str, required=True, help="This field cannot be left blank")
+        parser.add_argument("surname", type=str, required=True, help="This field cannot be left blank")
+        # parser.add_argument("location", type=str, required=True, help="This field cannot be left blank")
+
+        return parser.parse_args()
+
---
+++ tests/functional/test_profile.py
@@ -0,0 +1,60 @@
+import base64
+url = "http://127.0.0.1:5000/"
+
+
+def test_get_profile(_app, client, auth_header):
+    with client:
+        json = {'email': 'pepe432@gmail.com', 'username': 'pepeman', 'password': 'pepe123,.'}
+        login = client.post("API/login", json=json)
+        assert login.status_code == 200
+
+        # Test an invalid logout with nonexistent email
+        token = login.json
+        headers = {
+            'Authorization': 'Basic {}'.format(
+                base64.b64encode(
+                    '{token}:{password}'.format(
+                        token=token['token'],
+                        password='').encode()
+                ).decode()
+            )
+        }
+        r = client.get("API/profile/pepe433@gmail.com", headers=headers)
+        assert r.status_code == 404
+        assert r.json == {'message': 'This email [pepe433@gmail.com] does not exist'}
+
+        # Test invalid access with non-matching usernames between current user and request user
+        r = client.get("API/profile/killer23@gmail.com", headers=headers)
+        assert r.status_code == 401
+        assert r.json == {'message': "Bad authorization user"}
+
+        r = client.get("API/account/pepe432@gmail.com", headers=headers)
+        expected = {
+            'account': {
+                'birthday': None,
+                'confirmed': True,
+                'email': 'pepe432@gmail.com',
+                'is_admin': 0,
+                'name': None,
+                'surname': None,
+                'username': 'pepeman'
+            }
+        }
+
+        assert r.status_code == 200
+        assert r.json == expected
+
+
+def test_post_profile(_app, client):
+    with client:
+        json = {'email': 'pepe433@gmail.com', 'image': 'imagen.png', 'name': 'Pepe', 'surname': 'Márquez',
+                'location': 'Barcelona'}
+        r = client.post("API/profile", json=json)
+        assert r.status_code == 404
+        assert r.json == {'message': 'This email [pepe433@gmail.com] does not exist'}
+
+        json = {'email': 'pepe432@gmail.com', 'image': 'imagen.png', 'name': 'Pepe', 'surname': 'Márquez',
+                'location': 'Barcelona'}
+        r = client.post("API/profile", json=json)
+        assert r.status_code == 200
+        assert r.json == {'message': "Profile updated successfully"}
