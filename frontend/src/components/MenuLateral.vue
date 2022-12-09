e594c5f8a08530718ae83fc156bff967eaa338bf
SirKaza <mcasanto14@alumnes.ub.edu>
2022-12-09 22:11:00 +0100

    menu funcional al fin XD

--- frontend/src/components/MenuLateral.vue
+++ frontend/src/components/MenuLateral.vue
@@ -1,21 +1,26 @@
 <template>
   <div id="menu_lateral">
-    <nav class="navbar bg-dark navbar-dark" style="width: 90%;
+    <nav class="navbar navbar-light border" style="width: 90%;
     position: absolute;
     top: -10px;
-    bottom: 0;">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Perfil</a>
+    bottom: 0;
+    background-color: #f8f9fa;
+    ">
+      <ul class="navbar-nav" style="height: 90%;">
+        <li class="nav-item mb-5">
+          <a style="color: #000000" class="nav-link h5" href="#profile"><img src="@/assets/icons/user.png" alt="User icon" style="width: 40px"/> Perfil </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Compras</a>
+        <li class="nav-item mb-4">
+          <a style="color: #000000" class="nav-link h6" href="#introducirlinkreseñas"><img src="@/assets/icons/review.png" alt="User icon" style="width: 30px"/> Reseñas </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Ventas</a>
+        <li class="nav-item mb-4">
+          <a style="color: #000000" class="nav-link h6" href="#introducirlinkcompras"><img src="@/assets/icons/buy.png" alt="User icon" style="width: 30px"/> Compras </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Favoritos</a>
+        <li class="nav-item mb-4">
+          <a style="color: #000000" class="nav-link h6" href="#introducirlinkcompras"><img src="@/assets/icons/sell.png" alt="User icon" style="width: 30px"/> Ventas </a>
+        </li>
+        <li class="nav-item mb-4">
+          <a style="color: #000000" class="nav-link h6" href="#introducirlinkfavoritos"><img src="@/assets/heart.png" alt="User icon" style="width: 30px"/> Favoritos </a>
         </li>
       </ul>
     </nav>
@@ -31,3 +36,15 @@ export default {
   }
 }
 </script>
+
+<style>
+
+.nav-item.mb-4:hover {
+  background-color: #e6e8eb;
+}
+
+.nav-item.mb-5:hover {
+  background-color: #e6e8eb;
+}
+
+</style>
